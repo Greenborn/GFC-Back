62da181f118f955569a9e26c3009f8d8b36119d1
berty <lucho.2012.tandil@gmail.com>
2021-09-15 17:44:23 -0300

    se agregan los controllers

---
+++ controllers/ProfileContestController.php
@@ -0,0 +1,11 @@
+<?php
+namespace app\controllers;
+
+use yii\rest\ActiveController;
+
+
+class ProfileContestController extends BaseController {
+
+    public $modelClass = 'app\models\ProfileContest';
+
+}
