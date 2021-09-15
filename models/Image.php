ca2846c8ba564b02a24e654ec31e9485f78c3ab0
berty <lucho.2012.tandil@gmail.com>
2021-09-15 17:32:26 -0300

    se agregaron los modelos

---
+++ models/Image.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "image".
+ *
+ * @property int $id
+ * @property string $code
+ * @property string $title
+ * @property int $profile_id
+ *
+ * @property ContestResult[] $contestResults
+ */
+class Image extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'image';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['code', 'title', 'profile_id'], 'required'],
+            [['profile_id'], 'integer'],
+            [['code'], 'string', 'max' => 20],
+            [['title'], 'string', 'max' => 45],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'code' => 'Code',
+            'title' => 'Title',
+            'profile_id' => 'Profile ID',
+        ];
+    }
+
+    /**
+     * Gets query for [[ContestResults]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getContestResults()
+    {
+        return $this->hasMany(ContestResult::className(), ['image_id' => 'id']);
+    }
+}
