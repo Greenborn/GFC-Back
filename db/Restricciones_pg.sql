8ed94a7972a0097765decb3142cd9504a832870f
Evy <cvega@alumnos.exa.unicen.edu.ar>
2021-10-25 21:17:38 -0300

    restricción anotar a concurso anterior

--- db/Restricciones_pg.sql
+++ db/Restricciones_pg.sql
@@ -37,3 +37,22 @@ CREATE TRIGGER tr_borrado_contest_section
 BEFORE DELETE
 ON contest_section
 FOR EACH ROW EXECUTE PROCEDURE fn_borrado_contest_section();
+
+CREATE OR REPLACE FUNCTION fn_ingreso_profile_contest() RETURNS Trigger AS $$
+DECLARE
+fecha DATE;
+BEGIN
+    SELECT c.end_date INTO fecha
+    FROM contest c
+    WHERE c.id = NEW.contest_id;
+    IF (fecha < date(now())) THEN
+     RAISE EXCEPTION 'No se puede ingresar un nuevo profile_contest pasado el tiempo límite del concurso';
+    END IF;
+RETURN NEW;
+END $$
+LANGUAGE 'plpgsql';
+
+CREATE TRIGGER tr_ingreso_profile_contest
+BEFORE INSERT
+ON profile_contest
+FOR EACH ROW EXECUTE PROCEDURE fn_ingreso_profile_contest();
