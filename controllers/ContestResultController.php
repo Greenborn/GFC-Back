53461dfd8420b24fb8147fc04ff8561115cf2a03
f3dk <fed.fr@hotmail.com>
2021-11-05 20:06:21 -0300

    filtrado de contestResults y profileContests para concursantes

--- controllers/ContestResultController.php
+++ controllers/ContestResultController.php
@@ -6,6 +6,7 @@ use yii\rest\ActiveController;
 use yii\data\ActiveDataProvider;
 
 use app\models\Profile;
+use app\models\Contest;
 
 class ContestResultController extends BaseController {
 
@@ -13,20 +14,33 @@ class ContestResultController extends BaseController {
 
     public function prepareDataProvider(){
         $user = Yii::$app->user->identity;
+        $esAdmin = $user->role_id == 1;
         $esDelegado = $user->role_id == 2;
 
         $query = $this->modelClass::find();
 
-        if ($esDelegado) {
+        if (!$esAdmin) {
             $query = $query->joinWith('image');
         }
 
         $query = $this->addFilterConditions($query);
-  
-        // $user = User::findIdentityByAccessToken($this->getAccessToken());
-        if ($esDelegado) { // delegado
-          $query = $query->andWhere( ['in', 'image.profile_id', Profile::find()->select('id')->where(['fotoclub_id' => $user->profile->fotoclub_id])] );
+
+        if (!$esAdmin) {
+          $cond = $esDelegado ? ['in', 'image.profile_id', Profile::find()->select('id')->where(['fotoclub_id' => $user->profile->fotoclub_id])] :
+            ['image.profile_id' => $user->profile_id];
+          $query->andWhere([
+            'or',
+            [ 'in', 'contest_id',  Contest::find()->select('id')->where(['>', 'extract(epoch from age(end_date))', 0])],
+            $cond
+          ]);
         }
+  
+        
+        // $query->andWhere($condition);
+        // $user = User::findIdentityByAccessToken($this->getAccessToken());
+        // if ($esDelegado) { // delegado
+        //   $query = $query->andWhere( ['in', 'image.profile_id', Profile::find()->select('id')->where(['fotoclub_id' => $user->profile->fotoclub_id])] );
+        // }
   
         return new ActiveDataProvider([
           'query' => $query->orderBy(['id' => SORT_ASC]),
--- controllers/ProfileContestController.php
+++ controllers/ProfileContestController.php
@@ -7,6 +7,7 @@ use yii\data\ActiveDataProvider;
 
 // use app\modules\v1\models\User;
 use app\models\Profile;
+use app\models\Contest;
 
 
 class ProfileContestController extends BaseController {
@@ -15,14 +16,25 @@ class ProfileContestController extends BaseController {
 
     public function prepareDataProvider(){
         $user = Yii::$app->user->identity;
+        $esAdmin = $user->role_id == 1;
         $esDelegado = $user->role_id == 2;
 
         $query = $this->modelClass::find();
 
         $query = $this->addFilterConditions($query);
   
-        if ($esDelegado) { // delegado
-          $query = $query->andWhere( ['in', 'profile_id', Profile::find()->select('id')->where(['fotoclub_id' => $user->profile->fotoclub_id])]);
+        // if ($esDelegado) { // delegado
+        //   $query = $query->andWhere( ['in', 'profile_id', Profile::find()->select('id')->where(['fotoclub_id' => $user->profile->fotoclub_id])]);
+        // }
+        
+        if (!$esAdmin) {
+          $cond = $esDelegado ? ['in', 'profile_id', Profile::find()->select('id')->where(['fotoclub_id' => $user->profile->fotoclub_id])] :
+            ['profile_id' => $user->profile_id];
+          $query->andWhere([
+            'or',
+            [ 'in', 'contest_id',  Contest::find()->select('id')->where(['>', 'extract(epoch from age(end_date))', 0])],
+            $cond
+          ]);
         }
   
         return new ActiveDataProvider([
