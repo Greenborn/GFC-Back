d99743138fc16a951832f9a63582dcbd96731c82
Evy <cvega@alumnos.exa.unicen.edu.ar>
2021-12-14 19:00:25 -0300

    RI img otro inscripto

--- controllers/ImageController.php
+++ controllers/ImageController.php
@@ -3,11 +3,33 @@ namespace app\controllers;
 
 use yii\rest\ActiveController;
 // use yii\web\UploadedFile;
+use Yii;
 
 
 class ImageController extends BaseController {
 
     public $modelClass = 'app\models\Image';
+
+     
+    public function checkAccess($action, $model = null, $params = [])
+    {
+      $paramsGet = Yii::$app->getRequest()->getBodyParams();
+      // throw ForbiddenHttpException if access should be denied
+      if ($action === 'create' ) {
+        if ($model == null) {
+          $prf_id = $paramsGet["profile_id"];
+          // $prf_id = $params["profile_id"];
+        } 
+          if (($prf_id !==  Yii::$app->user->identity->profile_id)
+          && Yii::$app->user->identity->role_id == 3)
+              throw new \yii\web\ForbiddenHttpException(sprintf('No puede inscribir porque no es su usuario. Usuario que quiere inscribir: %d, su usuario: %d',  $prf_id, Yii::$app->user->identity->profile_id));
+              if (($prf_id ==  Yii::$app->user->identity->profile_id)
+              && Yii::$app->user->identity->role_id == 1)
+              throw new \yii\web\ForbiddenHttpException(sprintf('No puede inscribirse un usuario administrador'));
+
+
+       }
+    }
 
     // public function actionUpload()
     // {
