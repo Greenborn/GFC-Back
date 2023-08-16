0c2d3b4e4d664aa4ff0b678ce79288a8d286fcef
Luciano <lucho.2012.tandil@gmail.com>
2023-08-16 15:21:36 -0300

    Se modifica el LogManager para crear un archivo por día, para que los archivos de logs no se vuelvan gigantes

--- utils/LogManager.php
+++ utils/LogManager.php
@@ -7,9 +7,9 @@ const LOGS_PATH    = BASE_PATH.RUNTIME_DIR;
 
 class LogManager {
     public static function toLog($cont, $file){
-        $fp = fopen( LOGS_PATH.$file, 'a');
-        if (!$fp) echo "Error leyendo archivo log";
         $fecha = new \DateTime();
+        $fp = fopen( LOGS_PATH.$file.$fecha->format('Y-m-d'), 'a');
+        if (!$fp) echo "Error leyendo archivo log";
         fwrite($fp, $fecha->format('Y-m-d H:i:s').json_encode($cont, JSON_UNESCAPED_SLASHES)."\n");
         fclose($fp);
     }
