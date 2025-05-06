ee22693db2b482e9b661be464ae2706c7fe3f28f
luciano <lucho.2012.tandil@gmail.com>
2025-05-06 16:40:57 -0300

    [#195]Se agrega endpoint que retorna listado de usuarios

--- node_api/auth.js
+++ node_api/auth.js
@@ -46,6 +46,16 @@ router.post('/cerrar-sesion', (req, res) => {
   });
 });
 
+router.get('/users', async (req, res) => {
+  try {
+    const registros = await global.knex('user')
+    res.json({ user_data:registros });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error al obtener registros' });
+  }
+});
+
 router.get('/log-operaciones', async (req, res) => {
   try {
     const registros = await global.knex('log_operaciones')
