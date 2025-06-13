6ff8ccf8b7fbadb7e033f9f7f3e6183b89ca5395
luciano <lucho.2012.tandil@gmail.com>
2025-06-13 05:53:30 -0300

    nodemon ahora no hace seguiemiento de directorio de sessiones lo que daba lugar a problemas en desarrollo

--- node_api/routes/auth.js
+++ node_api/routes/auth.js
@@ -31,8 +31,8 @@ router.post('/login', async (req, res) => {
     req.session.user = user;
     const token = crypto.randomBytes(32).toString('hex');
     req.session.token = token;
-    req.session.save()
     res.status(200).send({ r: true,  message: 'Login exitoso' });
+    req.session.save()
     return
   } catch (error) {
     console.error(error);
