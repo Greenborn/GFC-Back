6dd3a5b6d76d3b2a59c591e62ddbf55b0788c11b
luciano <lucho.2012.tandil@gmail.com>
2025-07-18 21:58:06 -0300

    fix: Corregir error 500 específico en endpoint /fotoclub con filtros - Mejorar FotoclubController con manejo de excepciones en prepareDataProvider - Agregar behaviors específicos para CORS en FotoclubController - Mejorar trait Filterable con manejo robusto de errores - Agregar validación de arrays y try-catch en filtros complejos - Logging de errores específicos para debugging

--- php_api/traits/Filterable.php
+++ php_api/traits/Filterable.php
@@ -2,50 +2,60 @@
 namespace app\traits;
 
 use Yii;
+use Exception;
 
 trait Filterable {
 
   function addFilterConditions($query) {
-    $params = Yii::$app->request->get('filter');
-    if (isset($params)){
-      foreach ($params as $key => $value) {
-        if (!is_array($value))
+    try {
+      $params = Yii::$app->request->get('filter');
+      if (isset($params) && is_array($params)){
+        foreach ($params as $key => $value) {
+          if (!is_array($value)) {
             $query->andFilterCompare($key, $value);
-         else{
-           $this->buildComplexConditions($query, $key, $value);
+          } else {
+            $this->buildComplexConditions($query, $key, $value);
+          }
         }
-
       }
+    } catch (Exception $e) {
+      error_log("Error en Filterable::addFilterConditions: " . $e->getMessage());
+      // En caso de error, continuar sin filtros
     }
 
     return $query;
   }
 
   public function buildComplexConditions($query, $value, $expressions){
-    foreach ($expressions as $key => $values) {
-      switch ($key) {
-        case 'between': // &filter[profile.id][between]=4,7
-          $expArray = explode(',', $values);
-          //se ordena para garantizar que el primer valor es menor
-          sort($expArray);
+    try {
+      foreach ($expressions as $key => $values) {
+        switch ($key) {
+          case 'between': // &filter[profile.id][between]=4,7
+            $expArray = explode(',', $values);
+            //se ordena para garantizar que el primer valor es menor
+            sort($expArray);
 
-          $query->andFilterCompare($value, '>='.$expArray[0]);
-          $query->andFilterCompare($value, '<='.$expArray[1]);
+            $query->andFilterCompare($value, '>='.$expArray[0]);
+            $query->andFilterCompare($value, '<='.$expArray[1]);
 
-          break;
-        case 'inside': // &filter[profile.id][inside]=4,7
-          $expArray = explode(',', $values);
-          //se ordena para garantizar que el primer valor es menor
-          sort($expArray);
-          $query->andFilterCompare($value, '>'.$expArray[0]);
-          $query->andFilterCompare($value, '<'.$expArray[1]);
+            break;
+          case 'inside': // &filter[profile.id][inside]=4,7
+            $expArray = explode(',', $values);
+            //se ordena para garantizar que el primer valor es menor
+            sort($expArray);
+            $query->andFilterCompare($value, '>'.$expArray[0]);
+            $query->andFilterCompare($value, '<'.$expArray[1]);
 
-        break;
-        case 'in': // &filter[profile.id][in]=4,6
-          $expArray = explode(',', $values);
-          $query->where(['in', $value,$expArray]);
-        break;
+            break;
+          case 'in': // &filter[profile.id][in]=4,6
+            $expArray = explode(',', $values);
+            $query->where(['in', $value,$expArray]);
+            break;
+        }
       }
+    } catch (Exception $e) {
+      error_log("Error en Filterable::buildComplexConditions: " . $e->getMessage());
+      // En caso de error, continuar sin condiciones complejas
     }
   }
 
