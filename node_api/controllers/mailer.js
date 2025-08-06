965bb490a219bd6271a173e667ff084350122fe4
luciano <lucho.2012.tandil@gmail.com>
2025-08-06 08:34:51 -0300

    Agrega token de notificador al payload de email usando variable de entorno TOKEN_NOTIFICADOR

--- node_api/controllers/mailer.js
+++ node_api/controllers/mailer.js
@@ -1,21 +1,24 @@
 const axios = require('axios')
 
 async function sendEmail(email_data) {
-  const url = global.config.email_service_url+'/email'
+  const url = global.config.email_service_url + '/email';
 
   const headers = {
     'Content-Type': 'application/json'
-  }
+  };
+
+  // Agregar el token al objeto email_data
+  email_data.token = global.config.TOKEN_NOTIFICADOR;
 
   try {
     const response = await axios.post(url, email_data, { headers });
-    const statusCode = response.status
+    const statusCode = response.status;
 
-    console.log(`Email enviado con código de estado ${statusCode}`)
-    return {'status': statusCode};
+    console.log(`Email enviado con código de estado ${statusCode}`);
+    return { status: statusCode };
   } catch (error) {
     console.error('Error al enviar email:', error);
-    return {'error': error};
+    return { error: error };
   }
 }
 
