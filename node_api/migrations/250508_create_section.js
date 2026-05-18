12616fca8b5fb19caf8d9e1d690a5ed0fdcadfd2
luciano <lucho.2012.tandil@gmail.com>
2026-05-18 05:52:53 -0300

    se arregla error de migracion

---
+++ node_api/migrations/250508_create_section.js
@@ -0,0 +1,7 @@
+exports.up = async (knex) => {
+  // Migration placeholder: la tabla section se crea en 250519_create_section.js
+};
+
+exports.down = async (knex) => {
+  // No-op para mantener compatibilidad con la secuencia de migraciones
+};
