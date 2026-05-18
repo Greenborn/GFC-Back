a7ec2a64d64e8d432861b6fdde074ed83d617331
luciano <lucho.2012.tandil@gmail.com>
2026-05-18 05:54:54 -0300

    se quitan miugraciones ya usadas con anterioridad

--- node_api/migrations/250508_create_section.js
+++
@@ -1,7 +0,0 @@
-exports.up = async (knex) => {
-  // Migration placeholder: la tabla section se crea en 250519_create_section.js
-};
-
-exports.down = async (knex) => {
-  // No-op para mantener compatibilidad con la secuencia de migraciones
-};
