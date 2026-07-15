fc039573686a1faf671b33259a18a2bfc4898d32
luciano <lucho.2012.tandil@gmail.com>
2026-07-15 07:45:49 -0300

    [#488] refactor(section): deshabilitar acción de actualización 'update' en SectionController para alinear con permisos de solo-lectura

--- php_api/controllers/SectionController.php
+++ php_api/controllers/SectionController.php
@@ -8,4 +8,11 @@ class SectionController extends BaseController {
 
     public $modelClass = 'app\models\Section';
 
+    public function actions()
+    {
+        $actions = parent::actions();
+        unset($actions['update']);
+        return $actions;
+    }
+
 }
