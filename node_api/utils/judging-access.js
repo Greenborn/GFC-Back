b91adc2e804c39083c42ef44806ae837ae4a14f6
luciano <lucho.2012.tandil@gmail.com>
2026-08-20 06:01:41 -0300

    [#503] Eliminar el bypass de admin en canJudge; ahora los admins deben estar explícitamente listados como jueces del concurso, forzando la asignación de jueces por concurso.

--- node_api/utils/judging-access.js
+++ node_api/utils/judging-access.js
@@ -1,5 +1,4 @@
 async function canJudge(req, contestId) {
-  if (String(req.user.role_id) === '1') return true;
   const juez = await global.knex('contest_judge')
     .where({ contest_id: contestId, user_id: req.user.id })
     .first();
